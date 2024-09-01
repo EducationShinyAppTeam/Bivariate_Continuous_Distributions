6c34a1f532259238f42896f76c190b94bb6121ba
npechulis <njp5739@psu.edu>
2024-09-01 17:27:17 -0400

    app basics

--- app.R
+++ app.R
@@ -4,6 +4,13 @@ library(shinydashboard)
 library(shinyBS)
 library(shinyWidgets)
 library(boastUtils)
+library(DT)
+library(ggplot2)
+library(tidyr)
+library(plotly)
+library(MASS)
+library(graphics)
+library(viridis)
 
 # Load additional dependencies and setup functions
 # source("global.R")
@@ -38,11 +45,9 @@ ui <- list(
         id = "pages",
         menuItem("Overview", tabName = "overview", icon = icon("gauge-high")),
         menuItem("Prerequisites", tabName = "prerequisites", icon = icon("book")),
-        menuItem("Example", tabName = "example", icon = icon("book-open-reader")),
-        menuItem("Explore", tabName = "explore", icon = icon("wpexplorer")),
+        menuItem("Joint vs Marginal", tabName = "explore1", icon = icon("wpexplorer")),
+        menuItem("Conditioning", tabName = "explore2", icon = icon("wpexplorer")),
         menuItem("Challenge", tabName = "challenge", icon = icon("gears")),
-        menuItem("Game", tabName = "game", icon = icon("gamepad")),
-        menuItem("Wizard", tabName = "wizard", icon = icon("hat-wizard")),
         menuItem("References", tabName = "references", icon = icon("leanpub"))
       ),
       tags$div(
@@ -57,7 +62,7 @@ ui <- list(
         tabItem(
           tabName = "overview",
           withMathJax(),
-          h1("Sample Application for BOAST Apps"), # This should be the full name.
+          h1("Bivariate Continuous Distributions"), # This should be the full name.
           p("This is a sample Shiny application for BOAST. Remember, this page
             will act like the front page (home page) of your app. Thus you will
             want to have this page catch attention and describe (in general terms)
@@ -98,7 +103,7 @@ ui <- list(
             citeApp(),
             br(),
             br(),
-            div(class = "updated", "Last Update: 11/8/2022 by NJH.")
+            div(class = "updated", "Last Update: 9/1/2024 by NP.")
           )
         ),
         #### Set up the Prerequisites Page ----
@@ -156,17 +161,108 @@ ui <- list(
         #### Note: you must have at least one of the following pages. You might
         #### have more than one type and/or more than one of the same type. This
         #### will be up to you and the goals for your app.
-        #### Set up an Explore Page ----
+        #### Set up joint vs marginals Page ----
         tabItem(
-          tabName = "explore",
+          tabName = "explore1",
           withMathJax(),
-          h2("Explore the Concept"),
-          p("This page should include something for the user to do, the more
-            active and engaging, the better. The purpose of this page is to help
-            the user build a productive understanding of the concept your app
-            is dedicated to."),
-          p("Common elements include graphs, sliders, buttons, etc."),
-          p("The following comes from the NHST Caveats App:"),
+          h2("Joint vs Marginal PDFs"),
+          br(),
+          p(tags$strong("Standard Normally Distributed 3d Density Graph")),
+          fluidRow(
+            column(
+              width = 8,
+              uiOutput("normPlot")
+            )
+          ),
+          br(),
+          br(),
+          p(tags$strong("Normal Graph w/ Correlation + Contour Map")),
+          fluidRow(
+            column(
+              width = 6,
+              uiOutput("corrPlot")
+            ),
+            column(
+              width = 6,
+              plotOutput("contourMap")
+            )
+          ),
+          br(),
+          fluidRow(
+            column(
+              width = 6,
+              wellPanel(
+                sliderInput(
+                  inputId = 'correlationSlider',
+                  label = 'Adjust the slider to change the correlation value p',
+                  min = -0.9,
+                  max = 0.9,
+                  value = 0,
+                  step = 0.01
+                ))
+            )
+          )
+        ),
+        #### Set up conditional Explore Page ----
+        tabItem(
+          tabName = "explore2",
+          withMathJax(),
+          h2("Conditioning"),
+          br(),
+          p(tags$strong("Working with Conditional Plane (Std. Norm)")),
+          fluidRow(
+            column(
+              width = 6,
+              uiOutput("condPlot3d")
+            ),
+            column(
+              width = 6,
+              plotOutput('condPlane')
+            )
+          ),
+          br(),
+          fluidRow(
+            column(
+              width = 6,
+              wellPanel(
+                sliderInput(
+                  inputId = 'condSlider',
+                  label = 'Adjust the slider to move the positioning of the conditional plane',
+                  min = -2.5,
+                  max = 2.5,
+                  value = 0,
+                  step = 0.1
+                ))
+            )
+          ),
+          br(),
+          br(),
+          p(tags$strong("Conditional Plane w/ Correlation Value (p = 0.7)")),
+          fluidRow(
+            column(
+              width = 6,
+              uiOutput("condCorr")
+            ),
+            column(
+              width = 6,
+              plotOutput('condCorrPlane')
+            )
+          ),
+          br(),
+          fluidRow(
+            column(
+              width = 6,
+              wellPanel(
+                sliderInput(
+                  inputId = 'condSlider2',
+                  label = 'Adjust the slider to move the positioning of the conditional plane',
+                  min = -2.5,
+                  max = 2.5,
+                  value = 0,
+                  step = 0.1
+                ))
+            )
+          )
         ),
         #### Set up a Challenge Page ----
         tabItem(
@@ -181,32 +277,11 @@ ui <- list(
             consider is to re-create the tools of the Exploration page and then
             a list of questions for the user to then answer.")
         ),
-        #### Set up a Game Page ----
-        tabItem(
-          tabName = "game",
-          withMathJax(),
-          h2("Practice/Test Yourself with [Type of Game]"),
-          p("On this type of page, you'll set up a game for the user to play.
-            Game types include Tic-Tac-Toe, Matching, and a version Hangman to
-            name a few. If you have ideas for new game type, please let us know.")
-        ),
-        #### Set up a Wizard Page ----
-        tabItem(
-          tabName = "wizard",
-          withMathJax(),
-          h2("Wizard"),
-          p("This page will have a series of inputs and questions for the user to
-            answer/work through in order to have the app create something. These
-            types of Activity pages are currently rare as we try to avoid
-            creating 'calculators' in the BOAST project.")
-        ),
         #### Set up the References Page ----
         tabItem(
           tabName = "references",
           withMathJax(),
           h2("References"),
-          p("You'll need to fill in this page with all of the appropriate
-            references for your app."),
           p(
             class = "hangingindent",
             "Bailey, E. (2015). shinyBS: Twitter bootstrap components for shiny.
@@ -238,6 +313,143 @@ server <- function(input, output, session) {
       )
     }
   )
+  
+  ## Create Explore Page Graphs ----
+  
+  # create standard norm functions
+  joint_normal <- function(x,y) {
+    (1 / (2 * pi)) * exp(-0.5 * (x^2 + y^2))
+  }
+  marg <- function(x) {
+    (1 / sqrt(2 * pi)) * exp(-0.5 * x^2)
+  }
+  
+  # create xy grid then expand to 3d and apply joint function
+  x <- seq(-4, 4, length.out = 50)
+  y <- seq(-4, 4, length.out = 50) 
+  grid <- expand.grid(x = x,y = y)
+  grid$z <- joint_normal(grid$x, grid$y)
+  z <- matrix(grid$z, nrow = length(x), ncol = length(y))
+  
+  #### normal 3d plot ----
+  output$normPlot <- renderUI({
+    plotlyObj <- plot_ly(x = x, y = y, z = z, type = 'surface', hoverinfo = 'x+y+z+text', hovertext = "Joint PDF") %>%
+      layout(scene = list(
+        zaxis = list(title = "Density", hoverformat = '.3f'),
+        xaxis = list(hoverformat = '.3f', tickvals = seq(-4,4,by = 2), ticktext = as.character(seq(-4,4,by = 2))),
+        yaxis = list(hoverformat = '.3f', tickvals = seq(-4,4,by = 2), ticktext = as.character(seq(-4,4,by = 2))),
+        camera = list(eye = list(x = -2.25, y = 0, z = 0))
+      ),
+      dragmode = FALSE) %>%
+      # add marginal paths and scale
+      add_paths(x = x, y = -4, z = (1 / sqrt(2 * pi)) * marg(x), hovertext = "Marginal PDF of X", name = 'Marginal PDF of X', line = list(color = 'black')) %>%
+      add_paths(x = -4, y = y, z = (1 / sqrt(2 * pi)) * marg(y), hovertext = "Marginal PDF of Y", name = 'Marginal PDF of Y', line = list(color = 'black'))
+    config(plotlyObj, displaylogo = FALSE, displayModeBar = TRUE,
+           modeBarButtonsToRemove = list('orbitRotation', 'tableRotation', 'pan3d', 'resetCameraLastSave3d', 'toImage'))
+  })
+  
+  # create correlated pdf function and grid
+  corr_joint <- function(x,y,p) {
+    (1 / ((2 * pi) * sqrt(1-p^2))) * exp(-0.5 * (x^2 + y^2 - 2*p * x * y) / (1-p^2))
+  }
+  
+  #### normal dist. w/ correlation value ----
+  output$corrPlot <- renderUI({
+    corr_grid <- expand.grid(x = x,y = y)
+    corr_grid$z <- corr_joint(grid$x, grid$y, input$correlationSlider)
+    corr_z <- matrix(corr_grid$z, nrow = length(x), ncol = length(y))
+    plot_ly(x = x, y = y, z = corr_z, type = 'surface', hoverinfo = 'x+y+z+text', hovertext = "Joint PDF") %>%
+      layout(scene = list(zaxis = list(title = "Density")))
+  })
+  
+  # create contour map
+  output$contourMap <- renderPlot({
+    corr_grid <- expand.grid(x = x,y = y)
+    corr_grid$z <- corr_joint(grid$x, grid$y, input$correlationSlider)
+    corr_z <- matrix(corr_grid$z, nrow = length(x), ncol = length(y))
+    filled.contour(x,y,corr_z, asp = 1, color.palette = viridis,
+                   plot.title = title(main = "Contour Map", xlab = 'x', ylab = 'y'))
+  })
+  
+  
+  #### conditional plot w/ plane ----
+  output$condPlot3d <- renderUI({
+    plotlyObj <- plot_ly(x = x, y = y, z = z, type = 'surface', showscale = FALSE, opacity = 0, colorscale = list(c(0, 1), c(boastPalette[8], boastPalette[8])),
+                         hoverinfo = 'x+y+z+text', hovertext = "Joint PDF") %>%
+      layout(scene = list(
+        zaxis = list(title = "Density", hoverformat = '.3f'),
+        xaxis = list(hoverformat = '.3f', tickvals = seq(-4,4,by = 2), ticktext = as.character(seq(-4,4,by = 2))),
+        yaxis = list(hoverformat = '.3f', tickvals = seq(-4,4,by = 2), ticktext = as.character(seq(-4,4,by = 2)))
+      ))
+    # create and add conditional plane
+    x_val <- matrix(input$condSlider, nrow = length(y), ncol = length(z))
+    z[z >= joint_normal(input$condSlider, y)] <- NA
+    plotlyObj <- plotlyObj %>% add_surface(
+      x = x_val,
+      y = y,
+      z = z,
+      type = 'surface',
+      opacity = 1,
+      showscale = FALSE,
+      hovertext = "Conditional Plane")
+    # add mesh effect
+    plotlyObj <- plotlyObj %>% add_surface(x = ~x, y = ~y, opacity = 0, showscale = FALSE, 
+                                           contours = list(
+                                             x = list(show = TRUE, color = 'grey30', width = 1, start = -4, end = 4, size = 0.4),
+                                             y = list(show = TRUE, color = 'grey30', width = 1, start = -4, end = 4, size = 0.4)
+                                           ))
+  })
+  
+  # plane subplot (std. norm)
+  output$condPlane <- renderPlot({
+    cond_z <- joint_normal(input$condSlider,y) / marg(input$condSlider)
+    ggplotObj <- ggplot(data = data.frame(y = y, cond_z = cond_z), 
+                        mapping = aes(x = y, y = cond_z)) +
+      geom_line()
+    ggplotObj
+  })
+  
+  
+  
+  #### conditional w/ correlation ----
+  output$condCorr <- renderUI({
+    grid2 <- expand.grid(x = x,y = y)
+    grid2$z <- corr_joint(grid2$x, grid2$y, p = 0.7)
+    z <- matrix(grid2$z, nrow = length(x), ncol = length(y))
+    plotlyObj <- plot_ly(x = x, y = y, z = z, type = 'surface', showscale = FALSE, opacity = 0, colorscale = list(c(0, 1), c(boastPalette[8], boastPalette[8])),
+                         hoverinfo = 'x+y+z+text', hovertext = "Joint PDF") %>%
+      layout(scene = list(
+        zaxis = list(title = "Density", hoverformat = '.3f'),
+        xaxis = list(hoverformat = '.3f', tickvals = seq(-4,4,by = 2), ticktext = as.character(seq(-4,4,by = 2))),
+        yaxis = list(hoverformat = '.3f', tickvals = seq(-4,4,by = 2), ticktext = as.character(seq(-4,4,by = 2)))
+      ))
+    # create and add conditional plane
+    x_val <- matrix(input$condSlider2, nrow = length(y), ncol = length(z))
+    z[z >= corr_joint(input$condSlider2, y, p = 0.7)] <- NA
+    plotlyObj <- plotlyObj %>% add_surface(
+      x = x_val,
+      y = y,
+      z = z,
+      type = 'surface',
+      opacity = 1,
+      showscale = FALSE,
+      hovertext = "Conditional Plane")
+    # add mesh effect
+    plotlyObj <- plotlyObj %>% add_surface(x = ~x, y = ~y, opacity = 0, showscale = FALSE, 
+                                           contours = list(
+                                             x = list(show = TRUE, color = 'grey30', width = 1, start = -4, end = 4, size = 0.4),
+                                             y = list(show = TRUE, color = 'grey30', width = 1, start = -4, end = 4, size = 0.4)
+                                           ))
+  })
+  
+  # plane subplot (w/ correlation)
+  output$condCorrPlane <- renderPlot({
+    cond_z <- corr_joint(input$condSlider2,y, p = 0.7) / marg(input$condSlider2)
+    ggplotObj <- ggplot(data = data.frame(y = y, cond_z = cond_z), 
+                        mapping = aes(x = y, y = cond_z)) +
+      geom_line()
+    ggplotObj
+  })
 
 
 }
